bc5ba811ccb6531b41687af9113a90efb6088e7d
ArthurLVDH <arthurlvdh@gmail.com>
2023-02-11 14:36:45 -0800

    got apis working

--- JavaScript Scripts/questionApi.js
+++ JavaScript Scripts/questionApi.js
@@ -1,14 +1,28 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://would-you-rather.p.rapidapi.com/wyr/random",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "",
-		"X-RapidAPI-Host": "would-you-rather.p.rapidapi.com"
+//old api
+/*const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '355d816507msh9ae1ebcba373e28p19091cjsne755bbb9cb55',
+		'X-RapidAPI-Host': 'would-you-rather.p.rapidapi.com'
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+function refreshQuestion() {
+	fetch('https://would-you-rather.p.rapidapi.com/wyr/random', options)
+		.then(response => response.json())
+		.then(json => document.getElementById("question").innerHTML = (json[0]["question"]))
+		.catch(err => console.error(err));
+}*/
+
+const options = {
+	method: 'GET'
+};
+
+function refreshQuestion() {
+	fetch('https://would-you-rather-api.abaanshanid.repl.co/', options)
+		.then(response => response.json())
+		.then(json => document.getElementById("question").innerHTML = (json["data"]))
+		.catch(err => console.error(err));
+}
+//jonah helped
+window.onload = refreshQuestion;
